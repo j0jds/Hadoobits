62726dd61e57b017f2e549cc29d9aac4db060ec7
j0jds <mrlucassilva7342@gmail.com>
2024-11-12 01:16:17 -0300

    Added frontend dashboard

---
+++ frontendDashboard/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Dashboard from "./Dashboard";
+
+const App = () => {
+  return <Dashboard />;
+};
+
+export default App;
