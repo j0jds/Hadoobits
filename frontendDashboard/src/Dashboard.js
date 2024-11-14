5558f6dbecb1c9a5b2be1de3f84ba18246a7b37a
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-11-14 18:25:37 -0300

    Dashboard tooltip update and  mocked up data

--- frontendDashboard/src/Dashboard.js
+++ frontendDashboard/src/Dashboard.js
@@ -29,8 +29,7 @@ import {
 } from "recharts";
 import tasks from "./mockData";
 import "./styles.css"; // Import the CSS file
-import Layout from "./Layout.js"; 
-
+import Layout from "./Layout.js";
 
 const Dashboard = () => {
   const [charts, setCharts] = useState([]);
@@ -89,162 +88,197 @@ const Dashboard = () => {
   };
 
   return (
-    <Layout> {/* conteúdo com o Layout */}
-    <Container id="dashboard-container">
-      <Typography variant="h4" gutterBottom>
-        Task Dashboard
-      </Typography>
+    <Layout>
+      {" "}
+      {/* conteúdo com o Layout */}
+      <Container id="dashboard-container">
+        <Typography variant="h4" gutterBottom>
+          Task Dashboard
+        </Typography>
 
-      <FormControl fullWidth variant="outlined" margin="normal">
-        <InputLabel className="input-label">Chart Type</InputLabel>
-        <Select
-          value={chartType}
-          onChange={(e) => setChartType(e.target.value)}
-          label="Chart Type"
-          className="chart-select"
+        <FormControl fullWidth variant="outlined" margin="normal">
+          <InputLabel className="input-label">Chart Type</InputLabel>
+          <Select
+            value={chartType}
+            onChange={(e) => setChartType(e.target.value)}
+            label="Chart Type"
+            className="chart-select"
+          >
+            <MenuItem value="Pie">Pie Chart</MenuItem>
+            <MenuItem value="Bar">Bar Chart</MenuItem>
+            <MenuItem value="Line">Line Chart</MenuItem>
+            <MenuItem value="Area">Area Chart</MenuItem>
+            <MenuItem value="Radar">Radar Chart</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth variant="outlined" margin="normal">
+          <InputLabel className="input-label">Filter By</InputLabel>
+          <Select
+            value={filterType}
+            onChange={(e) => setFilterType(e.target.value)}
+            label="Filter By"
+            className="filter-select"
+          >
+            <MenuItem value="type">Task Type</MenuItem>
+            <MenuItem value="difficulty">Task Difficulty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={addChart}
+          className="add-chart-button"
         >
-          <MenuItem value="Pie">Pie Chart</MenuItem>
-          <MenuItem value="Bar">Bar Chart</MenuItem>
-          <MenuItem value="Line">Line Chart</MenuItem>
-          <MenuItem value="Area">Area Chart</MenuItem>
-          <MenuItem value="Radar">Radar Chart</MenuItem>
-        </Select>
-      </FormControl>
+          Add Chart
+        </Button>
 
-      <FormControl fullWidth variant="outlined" margin="normal">
-        <InputLabel className="input-label">Filter By</InputLabel>
-        <Select
-          value={filterType}
-          onChange={(e) => setFilterType(e.target.value)}
-          label="Filter By"
-          className="filter-select"
-        >
-          <MenuItem value="type">Task Type</MenuItem>
-          <MenuItem value="difficulty">Task Difficulty</MenuItem>
-        </Select>
-      </FormControl>
-
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={addChart}
-        className="add-chart-button"
-      >
-        Add Chart
-      </Button>
-
-      <Grid container spacing={3}>
-        {charts.map((chart, index) => (
-          <Grid item xs={12} md={6} key={index}>
-            <Paper elevation={3} className="chart-paper">
-              <Typography variant="h6">
-                {chart.type} Distribution by{" "}
-                {chart.filter === "type" ? "Task Type" : "Task Difficulty"}
-              </Typography>
-              {chart.type === "Pie" ? (
-                <PieChart width={300} height={300}>
-                  <Pie
-                    data={chart.data.pieData}
-                    cx="50%"
-                    cy="50%"
-                    outerRadius={80}
-                    fill="#8884d8"
-                    dataKey="value"
-                    label={{ fill: "white" }}
+        <Grid container spacing={3}>
+          {charts.map((chart, index) => (
+            <Grid item xs={12} md={6} key={index}>
+              <Paper elevation={3} className="chart-paper">
+                <Typography variant="h6">
+                  {chart.type} Distribution by{" "}
+                  {chart.filter === "type" ? "Task Type" : "Task Difficulty"}
+                </Typography>
+                {chart.type === "Pie" ? (
+                  <PieChart width={300} height={300}>
+                    <Pie
+                      data={chart.data.pieData}
+                      cx="50%"
+                      cy="50%"
+                      outerRadius={80}
+                      fill="#8884d8"
+                      dataKey="value"
+                      label={{ fill: "white" }}
+                    >
+                      {chart.data.pieData.map((entry, index) => (
+                        <Cell
+                          key={`cell-${index}`}
+                          fill={
+                            ["#3f51b5", "#9c27b0", "#ff4081", "#00e5ff"][
+                              index % 4
+                            ]
+                          }
+                        />
+                      ))}
+                    </Pie>
+                    <Tooltip
+                      formatter={(value, name, props) => [
+                        `${value}`,
+                        props.payload.name,
+                      ]}
+                    />
+                  </PieChart>
+                ) : chart.type === "Bar" ? (
+                  <BarChart width={300} height={300} data={chart.data.barData}>
+                    <XAxis dataKey="name" stroke="white" />
+                    <YAxis stroke="white" />
+                    <Tooltip
+                      formatter={(value, name, props) => [
+                        `${value}`,
+                        props.payload.name,
+                      ]}
+                    />
+                    <Legend />
+                    <Bar
+                      dataKey="tasks"
+                      fill="#3f51b5"
+                      label={{ fill: "white" }}
+                    />
+                  </BarChart>
+                ) : chart.type === "Line" ? (
+                  <LineChart
+                    width={300}
+                    height={300}
+                    data={chart.data.lineData}
                   >
-                    {chart.data.pieData.map((entry, index) => (
-                      <Cell
-                        key={`cell-${index}`}
-                        fill={
-                          ["#3f51b5", "#9c27b0", "#ff4081", "#00e5ff"][
-                            index % 4
-                          ]
-                        }
-                      />
-                    ))}
-                  </Pie>
-                  <Tooltip />
-                </PieChart>
-              ) : chart.type === "Bar" ? (
-                <BarChart width={300} height={300} data={chart.data.barData}>
-                  <XAxis dataKey="name" stroke="white" />
-                  <YAxis stroke="white" />
-                  <Tooltip />
-                  <Legend />
-                  <Bar
-                    dataKey="tasks"
-                    fill="#3f51b5"
-                    label={{ fill: "white" }}
-                  />
-                </BarChart>
-              ) : chart.type === "Line" ? (
-                <LineChart width={300} height={300} data={chart.data.lineData}>
-                  <XAxis dataKey="name" stroke="white" />
-                  <YAxis stroke="white" />
-                  <Tooltip />
-                  <Legend />
-                  <Line
-                    type="monotone"
-                    dataKey="uv"
-                    stroke="#3f51b5"
-                    label={{ fill: "white" }}
-                    dot={renderDot} // Use custom dot rendering
-                  />
-                </LineChart>
-              ) : chart.type === "Area" ? (
-                <AreaChart width={300} height={300} data={chart.data.areaData}>
-                  <XAxis dataKey="name" stroke="white" />
-                  <YAxis stroke="white" />
-                  <Tooltip />
-                  <Area
-                    type="monotone"
-                    dataKey="uv"
-                    fill="#3f51b5"
-                    label={{ fill: "white" }}
-                  />
-                </AreaChart>
-              ) : chart.type === "Radar" ? (
-                <RadialBarChart
-                  width={300}
-                  height={300}
-                  innerRadius="10%"
-                  outerRadius="80%"
-                  data={chart.data.radarData}
+                    <XAxis dataKey="name" stroke="white" />
+                    <YAxis stroke="white" />
+                    <Tooltip
+                      formatter={(value, name, props) => [
+                        `${value}`,
+                        props.payload.name,
+                      ]}
+                    />
+                    <Legend />
+                    <Line
+                      type="monotone"
+                      dataKey="uv"
+                      stroke="#3f51b5"
+                      label={{ fill: "white" }}
+                      dot={renderDot} // Use custom dot rendering
+                    />
+                  </LineChart>
+                ) : chart.type === "Area" ? (
+                  <AreaChart
+                    width={300}
+                    height={300}
+                    data={chart.data.areaData}
+                  >
+                    <XAxis dataKey="name" stroke="white" />
+                    <YAxis stroke="white" />
+                    <Tooltip
+                      formatter={(value, name, props) => [
+                        `${value}`,
+                        props.payload.name,
+                      ]}
+                    />
+                    <Area
+                      type="monotone"
+                      dataKey="uv"
+                      fill="#3f51b5"
+                      label={{ fill: "white" }}
+                    />
+                  </AreaChart>
+                ) : chart.type === "Radar" ? (
+                  <RadialBarChart
+                    width={300}
+                    height={300}
+                    innerRadius="10%"
+                    outerRadius="80%"
+                    data={chart.data.radarData}
+                  >
+                    <RadialBar
+                      minAngle={15}
+                      label={{ fill: "white", position: "inside" }}
+                      background
+                      dataKey="A"
+                    >
+                      {chart.data.radarData.map((entry, index) => (
+                        <Cell
+                          key={`cell-${index}`}
+                          fill={
+                            ["#3f51b5", "#9c27b0", "#ff4081", "#00e5ff"][
+                              index % 4
+                            ]
+                          }
+                        />
+                      ))}
+                    </RadialBar>
+                    <Tooltip
+                      formatter={(value, name, props) => [
+                        `${value}`,
+                        props.payload.subject,
+                      ]}
+                    />
+                  </RadialBarChart>
+                ) : null}
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  onClick={() => removeChart(index)}
+                  className="remove-chart-button"
                 >
-                  <RadialBar
-                    minAngle={15}
-                    label={{ fill: "white", position: "inside" }}
-                    background
-                    dataKey="A"
-                  >
-                    {chart.data.radarData.map((entry, index) => (
-                      <Cell
-                        key={`cell-${index}`}
-                        fill={
-                          ["#3f51b5", "#9c27b0", "#ff4081", "#00e5ff"][
-                            index % 4
-                          ]
-                        }
-                      />
-                    ))}
-                  </RadialBar>
-                  <Tooltip />
-                </RadialBarChart>
-              ) : null}
-              <Button
-                variant="outlined"
-                color="secondary"
-                onClick={() => removeChart(index)}
-                className="remove-chart-button"
-              >
-                Remove Chart
-              </Button>
-            </Paper>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
-    </Layout> 
+                  Remove Chart
+                </Button>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Layout>
   );
 };
 
